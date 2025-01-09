5b2b856e1ce8f40c70f78cc864d4ae63d331837e
doudoudoufff <danziyi9@gmail.com>
2025-01-09 15:28:24 +0800

    Initial Commit

---
+++ Flick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flick
+//
+//  Created by 豆子 on 2025/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
